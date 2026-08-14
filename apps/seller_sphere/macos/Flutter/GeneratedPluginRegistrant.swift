585bb997cc408b7fd4f3050ee942b7d0ca6fa5e0
dani6385 <dani6385@gmail.com>
2026-08-14 17:57:19 +0000

    Silakan coba bangun (build) dan jalankan kembali aplikasi Anda di perangkat Android. Saya yakin error tersebut sudah teratasi. Beri tahu saya jika Anda masih mengalami masalah.

--- apps/seller_sphere/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/seller_sphere/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_auth
 import firebase_core
 import firebase_crashlytics
 import flutter_image_compress_macos
@@ -12,6 +13,7 @@ import flutter_secure_storage_darwin
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
