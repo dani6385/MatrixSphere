7d91d4f1158966d3c2e64fccd6997486a7e8318b
dani6385 <dani6385@gmail.com>
2026-08-16 17:58:10 +0000

    Jika HomeScreen berhasil ditampilkan, berarti masalahnya memang ada pada proses inisialisasi atau perutean (routing) yang sebelumnya kita nonaktifkan. Jika layar masih putih keabu-abuan, berarti masalahnya ada di dalam HomeScreen atau dependensi yang digunakannya.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 
 import 'package:flutter/material.dart';
+//import 'package:matrix_sphere/navigations/app_router.dart';
+import 'package:shared_services/shared_services.dart';
+//import 'package:shared_ui/shared_ui.dart';
+import 'services/firebase_options.dart';
 import 'screens/homes/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppInitializer.initializeFirebase(
+      DefaultFirebaseOptions.currentPlatform);
   runApp(const MatrixSphere());
 }
 
@@ -11,9 +18,14 @@ class MatrixSphere extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Anda bisa membungkus dengan BlocProvider di sini
     return const MaterialApp(
       title: 'Matrix Sphere',
       home: HomeScreen(),
+    /*BaseApp(
+      title: 'Matrix Sphere',
+      routerConfig: appRouter,
+      themeMode: ThemeMode.system,*/
     );
   }
 }
