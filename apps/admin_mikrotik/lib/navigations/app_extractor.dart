145a0ffa358a7c9fbe22c7f0e7ec60da867b37f1
dani6385 <dani6385@gmail.com>
2026-08-18 01:09:05 +0700

    coba attendence saja

--- apps/admin_mikrotik/lib/navigations/app_extractor.dart
+++ apps/admin_mikrotik/lib/navigations/app_extractor.dart
@@ -1,5 +1,5 @@
 
-export 'package:admin_mikrotik/screens/homes/home_screen.dart';
+export 'package:admin_mikrotik/screens/home/home_screen.dart';
 export 'package:admin_mikrotik/screens/approvals/approval_screen.dart';
 export 'package:admin_mikrotik/screens/analytics/analytic_screen.dart';
 export 'package:admin_mikrotik/screens/transactions/transaction_screen.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:matrix_sphere/navigations/app_router.dart';
 import 'package:shared_services/shared_services.dart';
- import 'package:shared_ui/shared_ui.dart'; // BaseApp tidak digunakan untuk sementara
+import 'package:shared_ui/shared_ui.dart'; // BaseApp tidak digunakan untuk sementara
 import 'services/firebase_options.dart';
-//import 'screens/homes/home_screen.dart';
+import 'screens/attendance/attendance_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,16 +18,16 @@ class MatrixSphere extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Anda bisa membungkus dengan BlocProvider di sini
-    return /*MaterialApp.router(
+    return MaterialApp(
       title: 'Matrix Sphere',
-      routerConfig: appRouter,
+      home: const AttendanceScreen(),
       themeMode: ThemeMode.system,
       theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),*/
-    BaseApp(
+      darkTheme: ThemeData.dark(),
+        /*BaseApp(
       title: 'Matrix Sphere',
       routerConfig: appRouter,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.system,*/
     );
   }
 }
