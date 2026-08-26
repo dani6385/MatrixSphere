0ef77257461ec73ce811567d524fe7745f8a3d2c
dani6385 <dani6385@gmail.com>
2026-08-27 02:12:26 +0700

    coba

--- packages/shared_core/lib/shared_core.dart
+++ packages/shared_core/lib/shared_core.dart
@@ -2,3 +2,4 @@ export 'configs/app_type.dart';
 export 'configs/list_type.dart';
 export 'configs/page_type.dart';
 export 'package:shared_screens/shared_screens.dart';
+export 'package:shared_navigations/shared_navigations.dart'; // Sesuaikan dengan jalur paket navigasi Anda
---
+++ packages/shared_navigations/lib/shared_navigations.dart
@@ -0,0 +1,1 @@
+export 'navigation/app_navigation.dart';
